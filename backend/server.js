e6b9793347ec1f648d775df3f6dcbb314c0ace61
SergeyShemet <sergshemet@gmail.com>
2023-05-10 08:50:24 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,10 @@ require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  }
+)
 
 db.sequelize.sync()
     .then((result) => {
