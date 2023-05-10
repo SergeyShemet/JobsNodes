d0e3815ad98e195eb01c86b8ebe4523dacf8b47f
SergeyShemet <sergshemet@gmail.com>
2023-05-10 09:10:32 +0500

    fixes

--- frontend/src/components/card-group.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export const CardGroup = () => {
-    return (<>
-        <div className="card-group"></div>
-
-
-        </>
-    )
-}
--- frontend/src/components/card.component.jsx
+++ frontend/src/components/card.component.jsx
@@ -22,12 +22,7 @@ export const Card = (props) => {
     if (props.job.status === 2) cardSss = cardStyle[1];
     
     return (<>
-
         { showModal && <CardEditor setShowModal={setShowModal} job={props.job} isNew='0' currentUser={props.currentUser} refreshMode={props.refreshMode}/> }
-
-
-
-
         <div className="card" onClick={() => setShowModal(!showModal)}>
             <strong><h3><div className="card-header" style={cardSss}>{props.job.title}</div></h3></strong>
                 <div className="card-body">
@@ -35,8 +30,8 @@ export const Card = (props) => {
                 <p className="card-text">Приоритет: {jobPriorities[props.job.priority].text}</p>
                 <p className="card-text">Статус выполнения: {jobStatuses[props.job.status]}</p>
          </div>
-  <div className="card-footer my-0" style={{  marginBottom: '0' }} ><div>Выполнить до: {date}</div><span>Ответственный: {props.job.performer.lastName} {props.job.performer.firstName}</span></div>
-</div>
-        </>
+        <div className="card-footer my-0" style={{  marginBottom: '0' }} ><div>Выполнить до: {date}</div><span>Ответственный: {props.job.performer.lastName} {props.job.performer.firstName}</span></div>
+    </div>
+    </>
     )
 }
